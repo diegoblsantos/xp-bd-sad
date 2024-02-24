fd949ea0dd6e44fe848314b047dc03c4ee4aa86d
Diego <c0009150@vli-logistica.com.br>
2024-02-24 20:52:41 -0300

    empregados.sql

---
+++ aid_6/empregados.sql
@@ -0,0 +1,15 @@
+create external table empregados (
+	EMPREGADO_ID int, 
+	NOME string, 
+	SOBRENOME string, 
+	EMAIL string, 
+	CARGO_ID int, 
+	SALARIO decimal(10,2), 
+	GERENTE_ID string, 
+	DEPARTAMENTO_ID int
+)
+row format delimited 
+fields terminated by ',' 
+lines terminated by '\n' 
+location '/xp/bd/rh/empregados/'
+TBLPROPERTIES ("skip.header.line.count"="1");
